f782021b93fd04d2d29f566aa6d81d92567c97f2
Lucas91420 <118747855+Lucas91420@users.noreply.github.com>
2023-05-03 20:02:13 +0200

    Add files via upload

---
+++ Controller/Users_Controller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using gestiondeappartement.Modèle;
+
+namespace gestiondeappartement.Controller
+{
+    class Users_Controller
+    {
+        public static class GlobalVariables
+        {
+            public static AdherentDAO.Adherent UtilisateurConnecte { get; set; }
+        }
+    }
+}
